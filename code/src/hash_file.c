e2a9201411e4fff9fd52001f207f886baf2035ee
andhmak <sdi1900047@di.uoa.gr>
2021-12-11 19:02:10 +0200

    +

--- code/src/hash_file.c
+++ code/src/hash_file.c
@@ -150,6 +150,7 @@ HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
     CALL_BF(BF_GetBlock(fileDesc, i, block));
     IndexBlock* data = (IndexBlock*) BF_Block_GetData(block);
     for (int j = 0; j < INDEX_ARRAY_SIZE; j++){
+      printf("dataBlockCounter: %d, indexBlockAmount: %d, arraySize: %d\n", dataBlockCounter, indexBlockAmount, dataBlockCounter, arraySize);
       printf("Mapping %d to %d\n", i, dataBlockCounter);
       if (dataBlockCounter < indexBlockAmount + arraySize + 1) {
         data->index[j] = dataBlockCounter;
