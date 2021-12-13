d6bfd7d0dd6f532c85d39ac45e6b14147e18b556
andhmak <sdi1900047@di.uoa.gr>
2021-12-13 17:26:40 +0200

    +

--- code/src/hash_file.c
+++ code/src/hash_file.c
@@ -306,7 +306,7 @@ HT_ErrorCode HT_CloseFile(int indexDesc) {
     nextBlock = data->nextBlock;
     if (nextBlock == -1) {
       if (j < indexSize - 1) {
-        printf("making new block in %d\n", blockAmount++);
+        printf("making new block in %d\n", blockAmount);
         fflush(stdout);
         data->nextBlock = blockAmount++;
         BF_Block_SetDirty(block);
